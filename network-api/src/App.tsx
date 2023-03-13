a749ef99de1fda409d8e1d4cfb31f55a55d93e79
Nikola Mitrovic <n.mitrovic@vegait.rs>
2023-03-13 04:36:17 +0100

    Network API.

--- network-api/src/App.tsx
+++ network-api/src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider, Typography, Button } from "@material-ui/core";
+import { theme } from "./theme/theme";
+import PlanetList from "./components/PlanetList/PlanetList";
+import astronaut from "./assets/images/astronaut.png";
+import RocketLogo from "./components/RocketLogo/RocketLogo";
+import RocketTrail from "./assets/images/rocket-trail.png";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<RocketLogo />
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<div className="section">
+								<Typography
+									variant="h1"
+									className="title"
+									style={{ position: "relative", color: "white", marginTop: "100px" }}
+								>
+									Svemirko.rs
+									<img src={astronaut} className="astronaut" alt="astronaut" />
+								</Typography>
+								<Link
+									to="rocket"
+									style={{
+										display: "block",
+										margin: "20px 0 50px 0",
+										textAlign: "center",
+										textDecoration: "none",
+										fontWeight: "bold",
+									}}
+								>
+									<Button
+										variant="contained"
+										color="secondary"
+										style={{
+											margin: "auto",
+											color: "white",
+											textDecoration: "none",
+											borderRadius: "10px",
+											padding: "15px 20px",
+										}}
+									>
+										RESERVE A FLIGHT
+									</Button>
+								</Link>
+								<img src={RocketTrail} width="100%" />
+							</div>
+						}
+					/>
+					<Route path="/rocket" element={<PlanetList />} />
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
+	);
 }
 
 export default App;
